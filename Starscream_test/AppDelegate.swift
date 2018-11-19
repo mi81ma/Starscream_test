9b15c855ce1cb1a6f3dfd867f35efd54a7cf0944
masato <masato@m-mbp.local>
2018-11-19 14:05:05 +0800

    initial commit, set Picker View

--- Starscream_test/AppDelegate.swift
+++ Starscream_test/AppDelegate.swift
@@ -15,8 +15,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+
+        // ViewControllerのインスタンス
+        let firstView = ViewController()
+
+//        // Navigation Controller のインスタンス
+//        let navigationController = UINavigationController(rootViewController: firstView)
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        // set NavigationController to root View
+        self.window?.rootViewController = firstView
+
+        window?.backgroundColor = .white
+
+
+        window?.makeKeyAndVisible()
+
+
         return true
+    }
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -42,5 +61,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
-}
 
--- Starscream_test/ViewController.swift
+++ Starscream_test/ViewController.swift
@@ -8,12 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+
+    // UIPickerView.
+    private var myUIPicker: UIPickerView!
+
+    // 表示する値の配列.
+    private let myValues: NSArray = ["BCH/BTC", "ETH/BTC","LTC/BTC", "DASH/BTC", "ETC/BTC", "REP/BTC", "GNT/BTC", "XRP/BTC", "CVC/BTC", "BCH/ETC", "ETC/ETH", "REP/ETH", "GNT/ETH", "LTC/XRP", "DASH/XRP"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = #colorLiteral(red: 0.6140401363, green: 0.8325993419, blue: 0.7041608691, alpha: 1)
+
+        // UIPickerViewを生成.
+        myUIPicker = UIPickerView()
+
+        // サイズを指定する.
+        myUIPicker.frame = CGRect(x: 0, y: view.frame.height / 2, width: self.view.bounds.width, height: 300)
+
+        // Delegateを設定する.
+        myUIPicker.delegate = self
+
+        // DataSourceを設定する.
+        myUIPicker.dataSource = self
+
+        // Viewに追加する.
+        self.view.addSubview(myUIPicker)
     }
+
+
+    /*
+     Picker View Setting
+    */
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+
+     // pickerに表示する行数を返すデータソースメソッド(必須)
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myValues.count
+    }
+
+
+    //pickerに表示する値を返すデリゲートメソッド.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myValues[row] as? String
+    }
+
+
+     // pickerが選択された際に呼ばれるデリゲートメソッド.
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("row: \(row)")
+        print("value: \(myValues[row])")
+    }
+
 
 
 }
